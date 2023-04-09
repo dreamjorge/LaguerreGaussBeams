a0aa1ed941b10ae6a8b319c496c26d9066f97472
Jorge Ugalde <Jorge.Ugalde@continental-corporation.com>
2023-04-09 17:13:14 -0600

    changes for more readable solutions on Laguerre

--- pxlaguerregz.m
+++ pxlaguerregz.m
@@ -6,6 +6,6 @@ k=2*zl/wo^2;
     phiz=(2*nu+mu+1)*atan(z./zl);
 %     Truncamiento ligado al plano z=0;
 
-Xn=exp(-(x/(wo*sqrt(2*nu+mu+1))).^50).*XLaguerreG(97,nu,abs(mu),2*x.^2./(wz.^2)).*exp(1i*(k*x.^2./(2*Rz)-phiz));%./(wz.^(mu+1));
+Xn=exp(-(x/(wo*sqrt(2*nu+mu+1))).^50).*XLaguerreAssociated(nu,abs(mu),2*x.^2./(wz.^2)).*exp(1i*(k*x.^2./(2*Rz)-phiz));%./(wz.^(mu+1));
 Xn(isnan(Xn))=0;
 end
