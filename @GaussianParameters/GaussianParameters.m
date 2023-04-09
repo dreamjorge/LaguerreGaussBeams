716a2a172a800764b356f212c3128a40c0ede0b7
Jorge Ugalde <Jorge.Ugalde@continental-corporation.com>
2023-04-09 16:12:45 -0600

    parameters of beams

---
+++ @GaussianParameters/GaussianParameters.m
@@ -0,0 +1,191 @@
+classdef GaussianParameters <  handle & matlab.mixin.Copyable
+% This class gives Gaussian parameters of Gaussian Beam
+% recives zCoordinate,InitialWaist,Wavelength as input and
+% gives a object with next properties:
+%
+% -zCoordinate
+% -InitialWaist
+% -Wavelength
+% -RayleighDistance
+% -k
+% -Waist
+% -Radius
+% -GouyPhase
+% -Amplitude
+% -DivergenceAngle
+
+% Example: Parameters = GaussianParameters(zCoordinate,InitialWaist,Wavelength)
+
+  properties(GetAccess =private)
+    
+  end
+
+  properties
+    %% Independent properties
+    units            % physical units
+    zCoordinate      % z-coordinate (or distance of propagation)
+    initialWaist     % initial wasit of Gaussian Beam
+    wavelength       % wavelength of Gaussian Beam
+    
+  end
+  
+  properties (Dependent)
+    %% Properties dependent of Gaussian Beam
+    RayleighDistance % RayleighDistance.
+    k                % k-number.
+    waist            % Waist of Gaussian Beam at zCoordinate.
+    radius           % Radius of Gaussian Beam at zCoordinate.
+    GouyPhase        % GouyPhace of Gaussian Beam at zCoordinate.
+    amplitude        % Amplitude of Gaussian Beam at zCoordinate.
+    divergenceAngle  % Angle of Divergence of Gaussian Beam
+  end
+
+  methods(Static)
+
+    function waistZ = getwaist(zCoordinate,initialWaist,RayleighDistance) 
+        
+      waistZ = (initialWaist)*sqrt( (zCoordinate./RayleighDistance).^2 + 1);
+
+    end
+    
+    function phiZ = getphase(zCoordinate,RayleighDistance)
+        
+      phiZ = atan(zCoordinate/RayleighDistance);
+        
+    end
+    
+    function radiusZ = getradius(zCoordinate,RayleighDistance)
+        
+      radiusZ = (zCoordinate).*(1+(RayleighDistance./zCoordinate).^2);
+
+      if (find(isnan(radiusZ))~= 0)
+          radiusZ(isnan(radiusZ))= inf;
+      end
+      
+    end
+                                             
+  end
+  methods
+
+    function Parameters = GaussianParameters(NameValueArgs)
+      arguments
+        NameValueArgs.zCoordinate  (1,:) double {mustBeNumeric,mustBeReal}
+        NameValueArgs.initialWaist (1,1) double {mustBeNumeric,mustBeReal,mustBePositive}
+        NameValueArgs.wavelength   (1,1) double {mustBeNumeric,mustBeReal,mustBePositive}
+        NameValueArgs.units        (1,1) string
+      end
+     %% Constructor of Gaussian Beam   
+%       if nargin == 4 
+        Parameters.zCoordinate  = NameValueArgs.zCoordinate;
+        Parameters.initialWaist = NameValueArgs.initialWaist;
+        Parameters.wavelength   = NameValueArgs.wavelength;
+        Parameters.units        = NameValueArgs.units;
+%       else
+%          error('You need introduce zCoordinate, initialWaist, wavelength and units as inputs')
+%       end
+    end
+    %% Error for dependent properties
+    
+    function [] = set.RayleighDistance(Parameters,~)
+      fprintf('%s%d\n','RayleighDistance is: ',Parameters.RayleighDistance)
+      error('You cannot set RayleighDistance property'); 
+    end 
+   
+    function [] = set.k(Parameters,~)
+      fprintf('%s%d\n','k is: ',Parameters.k)
+      error('You cannot set k property'); 
+    end 
+    
+    function [] = set.waist(Parameters,~)
+      fprintf('%s%d\n','waist is: ',Parameters.Waist)
+      error('You cannot set Waist property'); 
+    end 
+        
+    function [] = set.radius(Parameters,~)
+      fprintf('%s%d\n','radius is: ',Parameters.Radius)
+      error('You cannot set Waist property'); 
+    end 
+       
+    function [] = set.GouyPhase(Parameters,~)
+      fprintf('%s%d\n','phase is: ',Parameters.PhiPhase)
+      error('You cannot set PhiPhase property'); 
+    end 
+    
+    function [] = set.amplitude(Parameters,~)
+      fprintf('%s%d\n','amplitude is: ',Parameters.Amplitude)
+      error('You cannot set Amplitude property'); 
+
+    end 
+    
+    
+    %% Get dependent properties
+    
+    function k = get.k(Parameters)
+       k  = (2*pi)/Parameters.wavelength;    
+    end
+    
+    function rayleighDistance = get.RayleighDistance(Parameters) 
+      rayleighDistance  = ((Parameters.initialWaist).^2).*(Parameters.k)/2;    
+    end
+    
+    function Waist = get.waist(Parameters)
+      Waist = GaussianParameters.getwaist(Parameters.zCoordinate,...
+                                          Parameters.initialWaist,...
+                                          Parameters.RayleighDistance);
+    end
+    
+    function Phase = get.GouyPhase(Parameters)
+      Phase = GaussianParameters.getphase(Parameters.zCoordinate,...
+                                          Parameters.RayleighDistance);
+    end
+    
+    function Radius = get.radius(Parameters)
+      Radius = GaussianParameters.getradius(Parameters.zCoordinate,...
+                                            Parameters.RayleighDistance);
+    end
+    
+    function Amplitude = get.amplitude(Parameters)
+      Amplitude = 1./Parameters.waist;
+    end
+    
+    function DivergenceAngle = get.divergenceAngle(Parameters)
+      DivergenceAngle = atan((Parameters.initialWaist)./(Parameters.RayleighDistance));
+    end
+
+    function [fig] = plotParameters(Parameters)
+    %% Function plots parameters of Gaussian Beam (Waist,DivergenceAngle)
+    % Input:
+    %  -GB Parameters as GaussianBeamParameters
+      fig=figure;
+      fig.Position=[320 263 1236 420];
+      p1 = plot( Parameters.zCoordinate...
+               , Parameters.waist...
+               ,'Color','red' );
+      hold on
+      p2 = plot( Parameters.zCoordinate...
+               ,-Parameters.waist...
+               ,'Color','red' );  
+      p3 = plot( Parameters.zCoordinate...
+               , Parameters.zCoordinate*tan(Parameters.divergenceAngle)...
+               ,'Color','blue');
+      p4 = plot( Parameters.zCoordinate...
+               ,-Parameters.zCoordinate*tan(Parameters.divergenceAngle)...
+               ,'Color','blue');
+
+      p5 = plot( Parameters.zCoordinate,...
+                 Parameters.radius);      
+      xlabel(['Distance of Propagation [',Parameters.units,']']);
+      ylabel(['[',Parameters.units,']']);
+      title('Parameters of Gaussian Beam');
+      p1legend = 'Waist of Gaussian Beam';
+      p3legend = ['Angle of Divergence = ',num2str(rad2deg(Parameters.divergenceAngle)),'°'];
+      legend([p1,p3],{p1legend,p3legend});
+      ylim([-250 250]);
+    end
+
+    
+    
+  end
+  
+
+end
---
+++ @LaguerreParameters/LaguerreParameters.m
@@ -0,0 +1,120 @@
+classdef LaguerreParameters < GaussianParameters & handle & matlab.mixin.Copyable
+% LaguerreParameters class gives Laguerre parameters of Laguerre Gaussian Beam
+% recives zCoordinate,InitialWaist,Wavelength,l,p as input and
+% gives a object with next properties:
+%
+% -l
+% -p
+% -LaguerreWaist
+% -PhiPhase
+% -zCoordinate
+% -InitialWaist
+% -Wavelength
+% -RayleighDistance
+% -k
+% -Waist
+% -Radius
+% -GouyPhase
+% -Amplitude
+% -DivergenceAngle
+% 
+
+% Example: Parameters = LaguerreParameters(zCoordinate...
+%                                         ,InitialWaist...
+%                                         ,Wavelength)
+  
+  properties
+    l % angular number
+    p % radial number
+  end
+  
+  properties (Dependent)
+    laguerreWaist  % Waist of Laguerre Gauss Beam
+    phiPhase       % Phase of Laguerre Gauss Beam
+  end
+  
+  methods(Static)
+      
+    function waistL = getwaist(zCoordinate,InitialWaist,RayleighDistance,p,l)
+    %%Function for estimate wast of Laguerre Gauss Beam
+      waistL = sqrt((2*p+l+1))...
+             .*(InitialWaist).*sqrt((zCoordinate./RayleighDistance).^2+1);  
+    end
+     
+
+    
+  end
+  
+  
+  methods
+    
+    function PhiPhase      = get.phiPhase(obj) 
+      % Function for estimate phase of Laguerre Gaussian Beam
+      PhiPhase = (abs(obj.l)+2*(obj.p)-1).*obj.GouyPhase; 
+    end
+
+    function LaguerreWaist = get.laguerreWaist(obj)
+      % Function for estimate phase of Laguerre Gaussian Beam
+      LaguerreWaist = LaguerreParameters.getwaist(obj.zCoordinate,...
+                                                  obj.initialWaist,obj.RayleighDistance,...
+                                                  obj.l,...
+                                                  obj.p);
+    end
+
+    
+    function Parameters = LaguerreParameters(zCoordinate, ...
+                                             initialWaist, ...
+                                             wavelength, ...
+                                             l, ...
+                                             p, ...
+                                             units)
+      %% LaguerreParameters Object
+      % generation of object with arguments inputs
+      
+      arguments
+        zCoordinate   (1,:) double {mustBeNumeric,mustBeReal}
+        initialWaist  (1,1) double {mustBeNumeric,mustBeReal,mustBePositive}
+        wavelength    (1,1) double {mustBeNumeric,mustBeReal,mustBePositive}
+        l             (1,1) double {mustBeNumeric,mustBeReal,mustBeInteger}
+        p             (1,1) double {mustBeNumeric,mustBeReal,mustBeInteger}
+        units         (1,1) string
+      end
+      %Call Gaussian Parameters
+      Parameters@GaussianParameters('zCoordinate', zCoordinate,...
+                                    'initialWaist',initialWaist,...
+                                    'wavelength',  wavelength,...
+                                    'units',       units);
+
+      % Add l,p parameters of input to object
+      Parameters.l = l;
+      Parameters.p = p;
+
+
+    end
+    
+    
+    function [] = plotParameters(obj)
+      %% Function plots parameters of Laguerre Gaussian Beam
+      % Input:
+      %  
+      p1            = plot(obj.zCoordinate ,obj.waist,'Color','red');
+      hold on
+      p2            = plot(obj.zCoordinate,-obj.waist,'Color','red');
+      p3            = plot(obj.zCoordinate, obj.zCoordinate*tan(obj.divergenceAngle),'Color','blue');
+      p4            = plot(obj.zCoordinate,-obj.zCoordinate*tan(obj.divergenceAngle),'Color','blue');
+      p5            = plot(obj.zCoordinate,-obj.laguerreWaist,'Color','green');
+      p6            = plot(obj.zCoordinate, obj.laguerreWaist,'Color','green');
+      xlabel(['Distance of Propagation on ',obj.units])
+      ylabel(['x on ',obj.units])
+      title('Parameters of Laguerre Gaussian Beam')
+      legend([p1,p5,p3],{'Waist of Gaussian Beam'...
+                        ,'Waist of Laguerre Beam'...
+                        ,['Angle of Divergence = ',num2str(rad2deg(obj.divergenceAngle)),'°']})
+      hold off
+      
+ 
+      
+      end
+
+   end
+end
