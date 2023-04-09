6d0137dac6020d3d6743b333d08437b73e57c95e
Jorge Ugalde <Jorge.Ugalde@continental-corporation.com>
2023-04-09 16:14:14 -0600

    update gradientrthz.m fix on indexes

--- gradientrthz.m
+++ gradientrthz.m
@@ -1,16 +1,20 @@
 function [mzr,mzth,mrth]=gradientrthz(fr,fth,fz,k,dr,dth,dz,r,th,z) 
-%funcion que calcula el gradiente en un punto dado 
-% f(x,y,z) a y,z constante en la primer componente
-% f(x,y,z) a x,z constante en la segunda componente
-% f(x,y,z) a x,y constante en la tercer componente
 
-%derivadas parciales
-gz=gradient(fz)/dz+k;
-gth = (1./r).*gradient(fth)./dth;
-gr=gradient(fr)/dr;
-N=size(gr,2);
-%pendientes
-mzr  = gz(floor(z/dz)+1)/gr(N/2+1+floor(r/dr));
-mzth = gz(floor(z/dz+1))    /gth(N/2+1+floor(th/dth));
-mrth = gr(N/2+1+floor(r/dr))/gth(N/2+1+floor(th/dth));
+    gz  = gradient(fz)/dz+k;
+    gth = (1./r).*gradient(fth)./dth;
+    gr  = gradient(fr)/dr;
+    
+    Nr  = size(gr,2);
+    Nth = size(gth,2);
+    Nz  = size(gz,2);
+    
+    index_r  = Nr/2 + 1 + floor(r/dr);
+    index_th = Nth/2 + 1 + floor(th/dth);
+    index_z  = floor(z/dz)+1;
+    
+    mzr  = gz(index_z)/gr(index_r);
+    mzth = gz(index_z)/gth(index_th);
+    mrth = gr(index_r)/gth(index_th);
+
+end
 
