589bb005f0899191f89536e8d620e4f01da857ff
Jorge Ugalde <jorge-kun@live.com>
2023-04-17 10:36:55 -0600

    update function for cylindrical2cartesian

--- cylindrical2cartesiangrad.m
+++ cylindrical2cartesiangrad.m
@@ -1,16 +1,10 @@
-function [grad_x, grad_y, grad_z] = cylindrical2cartesiangrad(grad_rho, grad_phi, grad_z, x, y, z)
+function gradcart = cylindrical2cartesiangrad(gradcyl, thi)
 % Convert gradient from cylindrical to Cartesian coordinates
 
-% Compute rho and phi from x, y
-rho = sqrt(x.^2 + y.^2);
-phi = atan2(y, x);
+    grad_x = gradcyl(1)*cos(thi) - gradcyl(2)*sin(thi);
+    grad_y = gradcyl(1)*sin(thi) + gradcyl(2)*cos(thi);
+    grad_z = gradcyl(3);
+    gradcart = [grad_x, grad_y, grad_z];
 
-% Compute conversion factors
-cos_phi = cos(phi);
-sin_phi = sin(phi);
-
-% Convert gradient to Cartesian coordinates
-grad_x = grad_rho .* cos_phi - grad_phi .* sin_phi;
-grad_y = grad_rho .* sin_phi + grad_phi .* cos_phi;
-grad_z = grad_z;
 end
+
